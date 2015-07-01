392430d98c5c2bb0d925101f48b7130e833e331e
Nicholas Potter <potter.nicholas@gmail.com>
2015-06-30 18:37:42 -0700

    add package meta-description

---
+++ R/napr.R
@@ -0,0 +1,10 @@
+#' @title napr-package.
+#'
+#' @description Functions and other bits of R code for frequent use.
+#'
+#' @author Nicholas Potter
+#' @docType package
+#' @name napr
+#' @aliases napr
+#'
+NULL
